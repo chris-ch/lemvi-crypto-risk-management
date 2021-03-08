6b61e6cb9905cf84a95839f9aec2267945f97353
Christophe <ch.alexandre@bluewin.ch>
2021-03-08 11:59:37 +0100

    dev

---
+++ src-local/google/cloud/storage.py
@@ -0,0 +1,6 @@
+class Client(object):
+    def list_blobs(self, bucket_name, prefix, delimiter):
+        pass
+
+    def bucket(self, bucket_name):
+        pass
--- src/subscribers.py
+++ src/subscribers.py
@@ -1,14 +1,21 @@
+import io
 import json
 import base64
-import os
+import logging
+
 from typing import Dict
 
+from google.cloud import storage
 from google.cloud.functions import Context
 from msgstore import FieldStoreFile
 
 
 def store_file(event: Dict, context: Context):
     """Background Cloud Function to be triggered by Pub/Sub.
+    Example data:
+    {
+    }
+
     Args:
          event (dict):  The dictionary with data specific to this type of
          event. The `data` field contains the PubsubMessage message. The
@@ -17,16 +24,24 @@ def store_file(event: Dict, context: Context):
          metadata. The `event_id` field contains the Pub/Sub message ID. The
          `timestamp` field contains the publish time.
     """
-
-    print("""This Function was triggered by messageId {} published at {}""".format(context.event_id, context.timestamp))
-
     if "data" not in event:
         return
 
     message = json.loads(base64.b64decode(event['data']).decode('utf-8'))
-    filename = message[FieldStoreFile.FILENAME]
+    filepath = message[FieldStoreFile.FILENAME]
+    path_items = filepath.split('/')
+    filename = path_items[-1]
+    file_prefix = '/'.join(path_items[:-1])
     content = message[FieldStoreFile.CONTENT]
     bucket_name = message[FieldStoreFile.BUCKET_NAME]
 
+    storage_client = storage.Client()
 
-    print("Hello {}!".format(message))
+    blobs = storage_client.list_blobs(bucket_name, prefix=file_prefix, delimiter='/')
+    if len([blob for blob in blobs if blob.name == filename]) > 0:
+        logging.error('file "{}" already found in bucket'.format(filepath))
+        return
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(filepath)
+    blob.upload_from_file(io.StringIO(content))
